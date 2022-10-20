cecf2f686c1c181a083e1e0558a15501703dca15
Manuel Antonio Aparicio Rosas <manuel@Manuels-MacBook-Air.local>
2022-10-20 23:45:18 +0200

    adding new file

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
